e6227941e01680381720c64b659f3a34bd779223
Alkgorth <ap.percheron@gmail.com>
2024-07-31 14:01:53 +0200

    install bootstrap

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes, chaque page créée sera ajouté ici pour indiquer le chemin
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/404", "404", "/pages/404.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
